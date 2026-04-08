42a4eab1f1822aa21a832e2c460ad2e6a862ff65
srikanth-ink <msrikanth-ink@gmail.com>
2026-04-08 11:29:26 +0530

    new updates

--- star patterns/SquarePattern.java
+++ star patterns/SquarePattern.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
-public class rectangle_pattern {
+public class SquarePattern {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int input = sc.nextInt();
+
+    // take number of rows as input
+    int input = (int) sc.nextInt();
     sc.close();
 
+    // outer loop to enter every row
     for (int i = 0; i < input; i++) {
+      // inner loop to enter every column
       for (int j = 0; j < input; j++) {
         System.out.print("*");
       }
