963a65610b7b758260c3181108f724eed9f00597
srikanth-ink <msrikanth-ink@gmail.com>
2026-04-09 08:07:09 +0530

    new pattern added: invered right triangle

---
+++ star patterns/InvertedRightTriangle.java
@@ -0,0 +1,35 @@
+/*
+
+input = 5
+
+sample output:
+
+*****
+****
+***
+**
+*
+
+*/
+
+import java.util.Scanner;
+
+public class InvertedRightTriangle {
+  public static void main(String[] args) {
+
+    // take the number of rows as input
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    // outer loop to iterate through each row
+    for (int i = 0; i < n; i++) {
+      // inner loop to print (n - current row number) stars
+      for (int j = 0; j < n - i; j++) {
+        System.out.print("*");
+      }
+      // move to next line after finishing a row
+      System.out.println();
+      sc.close();
+    }
+  }
+}
