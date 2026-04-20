30d089ef4069b947f76f35fa83f25ff2890dc303
srikanth <msrikanth.ink@gmail.com>
2026-04-20 18:07:07 +0530

    reverse integer code updated

--- math problems/ReverseInteger.java
+++ math problems/ReverseInteger.java
@@ -19,22 +19,25 @@ public class ReverseInteger {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-
-    int res = 0;
-
-    while (n != 0) {
-      int rem = n % 10;
-      n = n / 10;
-
-      // Check overflow BEFORE updating res
-      if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10) {
-        System.out.println(0);
-        return;
-      }
-
-      res = res * 10 + rem;
-    }
-
+    ReverseInteger obj = new ReverseInteger();
+    int res = obj.logic(n);
     System.out.println(res);
+  }
+
+  public static int logic(int n) {
+    int rem = 0;
+    int res = 0;
+    do {
+      rem = n % 10;
+      n = n / 10;
+      if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE && res > 7) {
+        return 0;
+      } else if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE && res < -8) {
+        return 0;
+      } else {
+        res = res * 10 + rem;
+      }
+    } while (n != 0);
+    return res;
   }
 }
